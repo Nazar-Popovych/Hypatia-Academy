4a801508f9429e7e690568a50881b96ef06ad513
Nazar <work@MacBook-Pro---Nazar.local>
2026-09-08 00:24:47 +0100

    Initial project setup and README

---
+++ src/(1)04_09_Nazar_Popovych.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int age;
+
+   age = 16; 
+
+   if (age % 2 == 0) { 
+
+    printf("Your age is even.\n"); 
+
+   }
+
+   else { 
+
+    printf("Your age is odd.\n");
+    
+   }
+}
---
+++ src/28_08_Nazar_Popovych.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("2 + 2 = %d\n", 2 + 2);
+
+    // or 
+
+    int a = 2; 
+
+    printf("%d + %d = %d\n", a, a, a + a);
+
+    return 0;
+}
+
